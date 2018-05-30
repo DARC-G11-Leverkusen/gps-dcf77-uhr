2f7e5f335aad862f8345db29bc1d2039b4a994a6
Shnalf <37964113+Shnalf@users.noreply.github.com>
2018-05-30 14:23:32 +0200

    Einbindung IntegerInEEPROM Library
    1. Entfernen von der EEPROM Integer lese- und schreibe-Funktion, da sie nun durch die IntegerInEEPROM Library zur Verfügung gestellt wird
    
    2. Einbindung von der IntegerInEEPROM Library
    
    3. Aktualisierung der Beschreibung des Hauptprogramms

--- Programm/Hauptprogramm.ino
+++ Programm/Hauptprogramm.ino
@@ -1,15 +1,13 @@
 /*
  *	Dies ist das Hauptprogramm des Projektes "Arduino Uhr" der Jugendgruppe
  *	des OVs G11 Leverkusen von IGEL e.V. und DARC e.V. .
- *
- *	Teile des Programms für den Zugriff auf den EEPROM sind von Matthias Busse.
  */
 
 #include <Arduino.h>
 #include <LiquidCrystal.h>
-#include <EEPROM.h>
 #include "RotaryEncoder.h"
 #include "MenueModul.h"
+#include "IntegerInEEPROM.h"
 
 
 ///////////////////////////////////////////////////////////////////////////////////
@@ -25,24 +23,24 @@ void loop(){
 	switch(menueAdresse){
 		//Adresse max. =2147483647
 		case   1:{
-			menueFuehrungState = HIGH;	//	Menü an/aus (HIGH = an)
+			menueFuehrungState = HIGH;		//	Menü an/aus (HIGH = an)
 			 menueRotaryEncoderState = 1;	/*  1: Rotary Encoder wird von MenueFuehrung() benutzt (standart)
-			 	 	 	 	 	 	 	 *  0: Rotary Encoder wird nicht von MenueFuehrung() benutzt (sondern z.B. für Auswahl von Infos)
-			 	 	 	 	 	 	 	 */
+			 	 	 	 	 	 	 	 	 *  0: Rotary Encoder wird nicht von MenueFuehrung() benutzt (sondern z.B. für Auswahl von Infos)
+			 	 	 	 	 	 	 	 	 */
 			 menueEintragSprung = LOW;	   	/*	Sprung bei Ende des Menüs zum ersten Eintrag, wenn heruntergedreht wird
-			 						   	 *	UND Sprung bei Anfang des Menüs zum letzten Eintrag, wenn heraufgedreht wird:
-			 							 *	an/aus (HIGH = an)
-			 							 */
+			 						   	 	 *	UND Sprung bei Anfang des Menüs zum letzten Eintrag, wenn heraufgedreht wird:
+			 						   	 	 *	an/aus (HIGH = an)
+			 						   	 	 */
 			 menueCursorZeichen[0] = "<";	//	Cursorzeichen (bei Auswahl von Infos z.B. "X")
-			 menueEintraegeAnzahl = 3;  //	Anzahl der Menüeinträge
+			 menueEintraegeAnzahl = 3;  	//	Anzahl der Menüeinträge
 
-			 menueEintrag[1] = "AA"; 	//  Inhalt vom Menüeintrag
-			 menueAktion[1] = 0;				/*	Aktion bei drücken Auf Menüeintrag ist...
-			 	 	 	 	 	 	 	 *	-1 = in nächst höhere Ebene/Adresse gehen
-			 	 	 	 	 	 	 	 *	  => !! ein "Zurück-Pfeil" ist bei MenueFuehrung() schon implementiert !!
-			 	 	 	 	 	 	 	 *	 0 = (noch) keine Aktion
-			 	 	 	 	 	 	 	 *	 1 = in nächst tiefere Ebene/Adresse gehen
-			 	 	 	 	 	 	 	 */
+			 menueEintrag[1] = "AA"; 		//  Inhalt vom Menüeintrag
+			 menueAktion[1] = 0;			/*	Aktion bei drücken Auf Menüeintrag ist...
+			 	 	 	 	 	 	 	 	 *	-1 = in nächst höhere Ebene/Adresse gehen
+			 	 	 	 	 	 	 	 	 *	  => !! ein "Zurück-Pfeil" ist bei MenueFuehrung() schon implementiert !!
+			 	 	 	 	 	 	 	 	 *	 0 = (noch) keine Aktion
+			 	 	 	 	 	 	 	 	 *	 1 = in nächst tiefere Ebene/Adresse gehen
+			 	 	 	 	 	 	 	 	 */
 			 menueEintrag[2] = "BBB";
 			 menueAktion[2] = 1;
 			 menueEintrag[3] = "C";
@@ -79,41 +77,7 @@ void loop(){
 ///////////////////////////////////////////////////////////////////////////////////
 
 
-int eepromReadInt(int adr) {
-  // 2 Byte Integer Zahl aus dem EEPROM lesen an der Adresse
-  // Eingabe:
-  //   adr: Adresse +0 und +1 wird gelesen
-  // Ausgabe: int Wert
-  //
-  // Matthias Busse 5.2014 V 1.0
 
-  byte low, high;
-
-  low=EEPROM.read(adr);
-  high=EEPROM.read(adr+1);
-  return low + ((high << 8)&0xFF00);
-}
-void eepromWriteInt(int adr, int wert) {
-  // 2 Byte Integer Zahl im EEPROM ablegen an der Adresse
-  // Eingabe:
-  //   adr: Adresse +0 und +1 wird geschrieben
-  //   wert: möglicher Wertebereich -32,768 bis 32,767
-  // Ausgabe:
-  //   -
-  // 2 Byte Platz werden belegt.
-  //
-  // Matthias Busse 5.2014 V 1.0
-  //
-  // (Speicher von Arduino UNO (ATmega328P) = 1024 Bytes)
-
-  byte low, high;
-
-  low=wert&0xFF;
-  high=(wert>>8)&0xFF;
-  EEPROM.write(adr, low); // dauert 3,3ms
-  EEPROM.write(adr+1, high);
-  return;
-}
 
 
 
