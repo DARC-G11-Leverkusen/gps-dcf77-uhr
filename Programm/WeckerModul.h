d23913a1ad16beda849dbfec35ca30391df017a7
Shnalf <himbeer.torte@gmx.de>
2018-06-08 18:58:25 +0200

    Wecker-Modul hinzugefügt (unfertig)

---
+++ Programm/WeckerModul.h
@@ -0,0 +1,24 @@
+/*
+ *	Wecker-Modul des Projektes "Arduino Uhr" der Jugendgruppe
+ *	des OVs G11 Leverkusen von IGEL e.V. und DARC e.V. .
+ *
+ * 	geschrieben von Ralf Rumbler, DO3KV 8.6.2018
+ */
+//*******************
+#ifndef WeckerModul_h
+#define WeckerModul_h
+
+#include <Arduino.h>
+
+char weckerDaten[5][5];
+int weckerAuswahl;
+byte weckerButtonStateA = LOW;
+
+#endif
+
+
+
+
+
+
+
