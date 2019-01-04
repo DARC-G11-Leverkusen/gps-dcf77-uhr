2f273a9b7ff04319dc4c26f9c68b152026cfd982
Shnalf <himbeer.torte@gmx.de>
2019-01-04 22:27:14 +0100

    Hinzufügung

---
+++ Programm/uebergreifendeProgramme.h
@@ -0,0 +1,20 @@
+/*
+ *	In dieser Datei befinden sich Programme, auf welche mehrere Module des
+ *	Projektes zugreifen.
+ *
+ *	Es ist Teil des Projektes "Arduino Uhr" der Jugendgruppe des OVs G11 Leverkusen
+ *	von IGEL e.V. und DARC e.V. .
+ */
+
+//************************
+
+#ifndef uebergreifendeProgramme_h
+#define uebergreifendeProgramme_h
+
+char itoaBufferA[12];
+char itoaBufferB[12];
+char itoaBufferC[12];
+char itoaBufferD[12];
+char itoaBufferE[12];
+
+#endif
